5d097ee882e37ce92671ca4a376da40eea3ff6a5
nshiba <naoya.shiba310@gmail.com>
2017-07-18 02:12:28 +0900

    Update multiform

--- src/main/kotlin/com/nshiba/api/controller/ApiController.kt
+++ src/main/kotlin/com/nshiba/api/controller/ApiController.kt
@@ -4,13 +4,14 @@ import com.nshiba.model.SensorCalculateModel
 import org.springframework.web.bind.annotation.*
 import org.springframework.web.multipart.MultipartFile
 import com.fasterxml.jackson.databind.ObjectMapper
+import com.nshiba.entity.CapturePointData
 import com.nshiba.model.AwsClient
 
 
 @RestController
 class ApiController {
 
-    private val filename = "test/data.json"
+    private val filename = "/test/data.json"
 
     private val awsClient = AwsClient()
 
@@ -21,16 +22,26 @@ class ApiController {
 
     @CrossOrigin(origins = arrayOf("http://localhost:3000"))
     @RequestMapping(path = arrayOf("/api/upload/csv"), method = arrayOf(RequestMethod.POST))
-    internal fun post(@RequestParam(name = "file") sensors: List<MultipartFile>): String? {
-        val model = SensorCalculateModel(sensors.map { String(it.bytes) })
+    internal fun post(@RequestParam(name = "file") sensors: List<MultipartFile>): Array<CapturePointData> {
+        val model = SensorCalculateModel(sensors.map { it.inputStream.reader().readText() })
         val uploadData = model.createData()
-        val json = ObjectMapper().writeValueAsString(uploadData)
-
-        return awsClient.putObject(filename, filename, json)
+        println(uploadData.toString())
+        return uploadData
+//        val json = ObjectMapper().writeValueAsString(uploadData)
+//
+//        println(json)
+//
+//        return awsClient.putObject(filename, json)
     }
 
     @RequestMapping(path = arrayOf("/api/projects"), method = arrayOf(RequestMethod.GET))
     internal fun fetchProject(): String {
         return awsClient.fetchProjectList()
+    }
+
+
+    @RequestMapping(path = arrayOf("/api/project/{name}"), method = arrayOf(RequestMethod.GET))
+    internal fun fetchProjectData(@PathVariable name: String): String {
+        return awsClient.fetchProjectData(name)
     }
 }
--- src/main/kotlin/com/nshiba/model/AwsClient.kt
+++ src/main/kotlin/com/nshiba/model/AwsClient.kt
@@ -5,8 +5,7 @@ import com.amazonaws.AmazonServiceException
 import com.amazonaws.auth.BasicAWSCredentials
 import com.amazonaws.services.s3.AmazonS3Client
 import com.amazonaws.services.s3.model.ListObjectsRequest
-import java.io.ByteArrayInputStream
-import java.io.IOException
+import java.io.*
 
 
 class AwsClient {
@@ -21,32 +20,9 @@ class AwsClient {
 
     private val bucketName = "mm360-video"
 
-    private val s3Client by lazy { createClient() }
+    private val rootDir = "projects"
 
-    fun putObject(filename: String, key: String, data: String): String {
-        return try {
-            val inputStream = ByteArrayInputStream(data.toByteArray())
-            s3Client.putObject(bucketName, key, inputStream, null)
-            "upload success"
-        } catch (ioe: IOException) {
-            "File IO Error: ${ioe.message}"
-        } catch (ase: AmazonServiceException) {
-            """Caught an AmazonServiceException,
-                    which means your request made it to Amazon S3,
-                    but was rejected with an error response for some reason.
-                    Error Message: ${ase.message},
-                    HTTP Status Code: ${ase.statusCode},
-                    AWS Error Code: ${ase.errorCode},
-                    Error Type: ${ase.errorType},
-                    Request ID: ${ase.requestId}"""
-        } catch (ace: AmazonClientException) {
-            """Caught an AmazonClientException,
-                    which means the client encountered
-                    an internal error while trying to communicate with S3,
-                    such as not being able to access the network.
-                    Error Message: ${ace.message}"""
-        }
-    }
+    private val s3Client by lazy { createClient() }
 
     private fun createClient(): AmazonS3Client {
         val credential = BasicAWSCredentials(ACCESS_KEY, SECRET_KEY)
@@ -54,15 +30,55 @@ class AwsClient {
         return client
     }
 
-    fun fetchProjectList(): String {
+    fun putObject(filename: String, data: String): String {
         return try {
-            val request = ListObjectsRequest(bucketName, "/", "/", null, 1000)
-            val listObjects = s3Client.listObjects(request)
-            listObjects.commonPrefixes.toString()
+            val inputStream = ByteArrayInputStream(data.toByteArray())
+            s3Client.putObject(bucketName, rootDir + filename, inputStream, null)
+            "upload success"
         } catch (ioe: IOException) {
             "File IO Error: ${ioe.message}"
         } catch (ase: AmazonServiceException) {
-            """Caught an AmazonServiceException,
+            showASEMessage(ase)
+        } catch (ace: AmazonClientException) {
+            showACEMessage(ace)
+        }
+    }
+
+    fun fetchProjectList(): String {
+        return try {
+            val request = ListObjectsRequest()
+                    .withBucketName(bucketName)
+                    .withPrefix("$rootDir/")
+                    .withDelimiter("/")
+            val listObjects = s3Client.listObjects(request).commonPrefixes
+
+            listObjects.map { it.replace("$rootDir/", "") }.toString()
+        } catch (ioe: IOException) {
+            "File IO Error: ${ioe.message}"
+        } catch (ase: AmazonServiceException) {
+            showASEMessage(ase)
+        } catch (ace: AmazonClientException) {
+            showACEMessage(ace)
+        }
+    }
+
+    fun fetchProjectData(projectName: String): String {
+        return try {
+            val s3Object = s3Client.getObject(bucketName, "$rootDir/$projectName/data.json")
+            val path = "$rootDir/$projectName/data.json"
+            println(path)
+            s3Object.objectContent.reader().readText()
+        } catch (ioe: IOException) {
+            "File IO Error: ${ioe.message}"
+        } catch (ase: AmazonServiceException) {
+            showASEMessage(ase)
+        } catch (ace: AmazonClientException) {
+            showACEMessage(ace)
+        }
+    }
+
+    private fun showASEMessage(ase: AmazonServiceException) =
+        """Caught an AmazonServiceException,
                     which means your request made it to Amazon S3,
                     but was rejected with an error response for some reason.
                     Error Message: ${ase.message},
@@ -70,12 +86,12 @@ class AwsClient {
                     AWS Error Code: ${ase.errorCode},
                     Error Type: ${ase.errorType},
                     Request ID: ${ase.requestId}"""
-        } catch (ace: AmazonClientException) {
+
+    private fun showACEMessage(ace: AmazonClientException) =
             """Caught an AmazonClientException,
                     which means the client encountered
                     an internal error while trying to communicate with S3,
                     such as not being able to access the network.
                     Error Message: ${ace.message}"""
-        }
-    }
+
 }
